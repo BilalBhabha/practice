1c9f61528c5cac29a899fb508a0a93fd05589008
BilalBhabha <108943300+BilalBhabha@users.noreply.github.com>
2022-09-01 13:08:50 -0700

    Hello world

---
+++ app.js
@@ -0,0 +1,91 @@
+// start cha 1
+// alert("Error! Please enter a valid password")
+
+// alert("Welcome to JS Land... \n Happy Coding")
+
+// alert("Wecome to JS Land...")
+// alert("Happy Coding! " )
+
+// alert("Hello... I can run JS through my web browser's console")
+
+// ending
+
+// start cha 2
+
+// var name ="Muhammad Bilal Bhabha";
+// var year ="15 year old";
+// var a ="Certified mobile Application Development";
+// alert (name);
+// alert(year);
+// alert(a);
+
+// alert(" Pizza\n Pizz\n Piz\n Pi\n P")
+
+// var email ="Muhammad Bilal Bhabaha@gmail .com";
+
+// alert(email)
+
+// var a ="I am trying to learn from the book A smarter\n way to learn JavaScript";
+// alert(a)
+
+// document.write("Yah! I can write HTML content through Javascript" );
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// ending
+
+// start cha 3
+
+// alert("15 year old")
+
+// alert("You have visited this site 14 times")
+
+// document.write("My birth year is 2007 ")
+// document.write("<br>");
+// document.write("Data type of my declared variable is number")
+
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+
+// ending
+
+// start cha 4
+// document.write("<h1>");
+// document.write("Rules for naming JS Variables")
+
+//  document.write("<br>");
+//  document.write("<br>");
+//  document.write("<h4>");
+//  document.write("Variable names can only contain, number, $and_ . For example : $my_1stvariable")
+//  document.write("<br>");
+// document.write("Variables must begin with a letter, $ or_. For example $name, _name or name");
+//   document.write("<br>");
+// document.write("Variable names are case snsitive");
+//  document.write("<br>");
+//  document.write("Variable names should not be JS keyword");
+
+// ending
+
+// start cha 5
+
+// document.write("<h1>");
+// document.write("Sum of 3 and 5 is 8");
+//  document.write("<h1>");
+// document.write("Value after variable declaration is undefined")
+// document.write("<br>");
+// document.write("Initial value: 5")
+// document.write("<br>");
+// document.write("Value after increment is: 13")
+// document.write("<br>");
+// document.write("Value after addition is: 13 ")
+// document.write("<br>");
+// document.write("Value after decrement is: 12")
+// document.write("<br>");
+// document.write("The remainder is : 0")
+
+var a = 600;
+var b = 5;
+var c =a*b;
+alert(c)
+document.write("<h1>");
+document.write("Total cost to buy 5 ticket to a movie is 3000PKR")
+// ending
